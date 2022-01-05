4a6310f8d976140d055e0e5835ca1ebd9b4fc150
MarkmanCZ <78849760+MarkmanCZ@users.noreply.github.com>
2022-01-05 17:42:10 +0100

    adding menu

--- 2DTileEditor/Form1.Designer.cs
+++ 2DTileEditor/Form1.Designer.cs
@@ -31,12 +31,19 @@ namespace _2DTileEditor
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mainMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.importButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.sizeChangeBtn = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(220, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(216, 27);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(800, 410);
             this.pictureBox1.TabIndex = 0;
@@ -44,25 +51,82 @@ namespace _2DTileEditor
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.canvasPaint);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainMenu,
+            this.settingsMenu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1264, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveButton,
+            this.importButton});
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(50, 20);
+            this.mainMenu.Text = "Menu";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(180, 22);
+            this.saveButton.Text = "Uložit";
+            // 
+            // importButton
+            // 
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(180, 22);
+            this.importButton.Text = "Importovat";
+            // 
+            // settingsMenu
+            // 
+            this.settingsMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sizeChangeBtn});
+            this.settingsMenu.Name = "settingsMenu";
+            this.settingsMenu.Size = new System.Drawing.Size(71, 20);
+            this.settingsMenu.Text = "Nastavení";
+            // 
+            // sizeChangeBtn
+            // 
+            this.sizeChangeBtn.Name = "sizeChangeBtn";
+            this.sizeChangeBtn.Size = new System.Drawing.Size(180, 22);
+            this.sizeChangeBtn.Text = "Velikost plochy";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.ClientSize = new System.Drawing.Size(1264, 482);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = this.Text;
             this.Text = "2D Tile Editor";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem saveButton;
+        private System.Windows.Forms.ToolStripMenuItem importButton;
+        private System.Windows.Forms.ToolStripMenuItem settingsMenu;
+        private System.Windows.Forms.ToolStripMenuItem sizeChangeBtn;
     }
 }
 
--- 2DTileEditor/Form1.cs
+++ 2DTileEditor/Form1.cs
@@ -22,15 +22,27 @@ namespace _2DTileEditor
         {
             int w = pictureBox1.Size.Width;
             int h = pictureBox1.Size.Height;
-            int cellSize = 64; //64x64 square
+            int cellSize = 50; //64x64 square
             Graphics g = e.Graphics;
             Pen p = new Pen(Color.Black);
+            int numOfCells = 14;
 
+            for (int y = 0; y < numOfCells; ++y)
+            {
+                g.DrawLine(p, 0, y * cellSize, numOfCells * cellSize, y * cellSize);
+            }
+
+
+            for (int x = 0; x < numOfCells; ++x)
+            {
+                g.DrawLine(p, x * cellSize, 0, x * cellSize, numOfCells * cellSize);
+            }
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             //
         }
+
     }
 }
